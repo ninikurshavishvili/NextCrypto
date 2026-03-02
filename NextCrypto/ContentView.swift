39ac4102ee4cf06271f40a936a50f452e8e61053
ninikurshavishvili <nino.kurshavishvili.1@btu.edu.ge>
2026-03-02 15:27:45 +0400

    Adding Project Images

--- NextCrypto/ContentView.swift
+++ NextCrypto/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
+    
+    
     var body: some View {
         NavigationSplitView {
             List {
